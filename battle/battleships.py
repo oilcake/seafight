977909eadb49ee08b20e18cb4ead3d5462b00999
oilcake <oilpie@gmail.com>
2021-10-21 10:22:03 +0300

    reset method clears log

--- battle/battleships.py
+++ battle/battleships.py
@@ -138,6 +138,8 @@ class Game:
         self.user.reset_sea()
         self.bot.reset_sea()
         self.state = 'idle'
+        del self.log
+        self.log = []
 
     def choose_action(self, message):
 
@@ -161,8 +163,8 @@ class Game:
             case 'fuck', *details:
                 self.reply = 'why are you so rude?'
             case ['reset']:
-                self.reply = 'ok, buy'
                 self.reset()
+                self.reply = 'ok, buy'
             case _:
                 self.reply = "can't get you. you are strange"
 
