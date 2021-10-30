1177e00a03bd9a7669308892dd689531531342c8
oilcake <oilpie@gmail.com>
2021-10-30 15:34:13 +0300

    channels layer in memory configured

--- battle/views.py
+++ battle/views.py
@@ -5,12 +5,19 @@ from .shipbuild import GRID_LETTERS
 
 user = 'You'
 
+# ht = json.dumps({game.user.sea}, default=serialize)
+# bt = json.dumps({game.bot.sea}, default=serialize)
+
+ht = game.user.sea
+bt = game.bot.sea
+
+text_data = {'human_tiles': ht,
+             'bot_tiles': bt,
+             'User': user,
+             'History': game.log,
+             'letters': GRID_LETTERS
+             }
+
 
 def battlefield(request):
-    return render(request, 'battle/battlefield.html',
-                  {'human_tiles': game.user.sea,
-                   'bot_tiles': game.bot.sea,
-                   'User': user,
-                   'History': game.log,
-                   'letters': GRID_LETTERS
-                   })
+    return render(request, 'battle/battlefield.html', text_data)
--- seafight/settings.py
+++ seafight/settings.py
@@ -75,14 +75,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'seafight.wsgi.application'
 ASGI_APPLICATION = 'seafight.asgi.application'
 
-# CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-#         'CONFIG': {
-#             "hosts": [('127.0.0.1', 6379)],
-#         },
-#     },
-# }
+CHANNEL_LAYERS = {
+    'default': {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
